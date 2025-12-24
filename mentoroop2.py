db0b1a7a4e6b42cedbfd190215fdf98252f29799
Marat <maratahmedov987@.com>
2025-12-24 15:11:44 +0300

    дописан код к 2 заданию

---
+++ mentoroop2.py
@@ -0,0 +1,72 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def rate_lecture(self, lecturer, course, grade):
+        if not isinstance(lecturer, Lecturer):
+            return 'Ошибка'
+        if course not in self.courses_in_progress:
+            return 'Ошибка'
+        if course not in lecturer.courses_attached:
+            return 'Ошибка'
+        if grade < 1 or grade > 10:
+            return 'Ошибка'
+        
+        if course in lecturer.grades:
+            lecturer.grades[course] += [grade]
+        else:
+            lecturer.grades[course] = [grade]
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+    def rate_hw(self, student, course, grade):
+        if not isinstance(student, Student):
+            return 'Ошибка'
+        if course not in self.courses_attached:
+            return 'Ошибка'
+        if course not in student.courses_in_progress:
+            return 'Ошибка'
+        if grade < 1 or grade > 10:
+            return 'Ошибка'
+        
+        if course in student.grades:
+            student.grades[course] += [grade]
+        else:
+            student.grades[course] = [grade]
+
+
+lecturer = Lecturer('Иван', 'Иванов')
+reviewer = Reviewer('Пётр', 'Петров')
+student = Student('Алёхина', 'Ольга', 'Ж')
+
+student.courses_in_progress += ['Python', 'Java']
+lecturer.courses_attached += ['Python', 'C++']
+reviewer.courses_attached += ['Python', 'C++']
+
+print(student.rate_lecture(lecturer, 'Python', 7))   # None
+print(student.rate_lecture(lecturer, 'Java', 8))     # Ошибка
+print(student.rate_lecture(lecturer, 'С++', 8))      # Ошибка  
+print(student.rate_lecture(reviewer, 'Python', 6))   # Ошибка
+
+print(lecturer.grades)  # {'Python': [7]}
